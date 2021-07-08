52413cc77f36c0a0dff9ccbf6969b50919cb1341
Scott Burton <scottburton11@gmail.com>
2021-07-07 17:35:43 -0700

    localize thet chat message text

--- scripts/lib/chat.js
+++ scripts/lib/chat.js
@@ -4,24 +4,28 @@ import {
 } from "./constants.js"
 
 
+const localize = (key) => {
+  return game.i18n.localize(`BURGER_TIME.${key}`)
+}
+
 export const hungerChatMessage = (actor, hungerIndex) => {
   const lastMealAt = game.Gametime.DTf({ seconds: Number(actor.getFlag('burger-time', 'lastMealAt')) })
 
   let rations
   rations = actor.data.items.find(item => item.name === game.settings.get('burger-time', 'rationName'))
 
-  const actionHtml = rations ? `Use ${game.settings.get('burger-time', 'rationName')} to regain your strength.` : `Find ${game.settings.get('burger-time', 'rationName')} soon!`
+  const actionHtml = rations ? `${localize('use').titleCase()} ${game.settings.get('burger-time', 'rationName')} ${localize('chat.to_regain_your_strength')}.` : `Find ${game.settings.get('burger-time', 'rationName')} soon!`
 
   const buttonsHtml = rations ? `<button data-action='consumeFood' data-item-id='${rations._id}' data-actor-id='${actor._id}'>Eat Now</button>` : `<button class='disabled'>No Rations</button>`
 
   const chatContent = `<div class='dnd5e chat-card'>
       <div class='card-header flexrow'>
         <img src="${HUNGER_ICONS[hungerIndex]}" title="Rations" width="36" height="36">
-        <h3>You are ${HUNGER_LEVELS[hungerIndex]}</h3>
+        <h3>${localize('chat.you_are')} ${HUNGER_LEVELS[hungerIndex]}</h3>
       </div>
       <div class='card-content'>
         <p>
-          You haven't eaten since <strong>${lastMealAt.longDate().date}</strong> at <strong>${lastMealAt.longDate().time}</strong>.
+          ${localize('chat.eaten_since')} <strong>${lastMealAt.longDate().date}</strong> ${localize('at')} <strong>${lastMealAt.longDate().time}</strong>.
         </p>
         <p>
           ${actionHtml}
@@ -32,8 +36,8 @@ export const hungerChatMessage = (actor, hungerIndex) => {
       </div>
       <div class='card-footer'>
         <span>${HUNGER_LEVELS[hungerIndex]}</span>
-        <span>Last Meal: ${lastMealAt.shortDate().time} on ${lastMealAt.shortDate().date}</span>
-        <span>Rations: ${rations ? rations.data.data.quantity : `None`}</span>
+        <span>Last Meal: ${lastMealAt.shortDate().time} ${localize('on')} ${lastMealAt.shortDate().date}</span>
+        <span>Rations: ${rations ? rations.data.data.quantity : localize('none').titleCase()}</span>
       </div>
     </div>`
 
@@ -42,7 +46,7 @@ export const hungerChatMessage = (actor, hungerIndex) => {
     type: CONST.CHAT_MESSAGE_TYPES.OTHER,
     speaker: { actor: actor._id },
     content: chatContent,
-    flavor: `Use ${game.settings.get('burger-time', 'rationName')} to satisfy your hunger.`,
+    flavor: `${localize('use').titleCase()} ${game.settings.get('burger-time', 'rationName')} ${localize('chat.to_satisfy_your_hunger')}.`,
     user: game.user._id
   })
 }
